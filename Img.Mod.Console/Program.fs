6acaba20413e4a650bb9716b16b244ff0334c400
Nabeel Valley <nabeelvalley@outlook.com>
2020-01-22 20:40:14 +0200

    :sparkles: basic image download and save

---
+++ Img.Mod.Console/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open Img.Mod.Lib
+open System.IO
+
+let url = "https://source.unsplash.com/random/800x600"
+let outputLocation = Path.GetTempPath() + Guid.NewGuid().ToString() + ".jpg"
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Location: %s" outputLocation
+
+    let image = File.GetImage url
+                |> Async.RunSynchronously
+                |> File.GetImageFromBytes
+
+    File.SaveAndDispose(image, outputLocation)
+
+    0
+        
---
+++ Img.Mod.Lib/File.fs
@@ -0,0 +1,30 @@
+﻿namespace Img.Mod.Lib
+
+open System.Net.Http
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.PixelFormats
+
+module File =
+    let GetImage url =
+        async {
+            printfn "Fetching %s" url
+            let client = new HttpClient()
+
+            let! res = client.GetAsync url
+                       |> Async.AwaitTask
+
+            let! content = res.Content.ReadAsByteArrayAsync()
+                          |> Async.AwaitTask
+
+            printfn "Done fetching %s" url
+
+            return content
+        }
+
+    let GetImageFromBytes (data: byte[]) =
+        Image.Load<Rgba32> data      
+
+    let SaveAndDispose (image:Image<Rgba32>, path) = 
+        image.Save(path)
+        printf("Image saved")
+
